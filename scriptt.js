010ad7c9d57a988e0a2367526eb9577436b3e83b
Polartica <109315921+Polartica@users.noreply.github.com>
2022-08-11 10:49:52 -0300

    Delete scriptt.js

--- scriptt.js
+++
@@ -1,7 +0,0 @@
-fetch("https://github.com/Polartica/coberturas/blob/main/TrioVerde.csv")
-.then(response => response.arrayBuffer())
-.then(ab => {
-  // do stuff with `ArrayBuffer` representation of file
-  console.log(ab)
-})
-.catch(err => console.log(err));
