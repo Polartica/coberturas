0f565b0b51384351bc91ac93a0597305471215fa
Polartica <109315921+Polartica@users.noreply.github.com>
2022-08-11 12:11:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ var soma = document.getElementById('Soma')
 var contagem = document.getElementById('Contagem')
 
 
+var myTableDiv = document.getElementById("myDynamicTable");
+  
+var table = document.createElement('TABLE');
+table.border = '1';
+
+var tableBody = document.getElementById("tableBody");
+table.appendChild(tableBody);
+
 function getvals(){
     return fetch("https://raw.githubusercontent.com/Polartica/coberturas/main/TrioVerde.csv",
     {method: "GET"})
@@ -20,21 +28,44 @@ function getvals(){
     data =  responseData.split('\n');
     len = data.length
 
-    div = getClientByCode(code.value, data, len)
-    show(div)
-
+    div = getClientBySetor(code.value, data, len)
     
+    for (var i = 0; i < div.length; i++) {
+        var tr = document.createElement('TR');
+        tableBody.appendChild(tr);
+        for (let x = 0; x < 10; x++) {
+            for (var j = 0; j < 1; j++) {
+                var td = document.createElement('TD');
+                td.width = '75';
+                td.appendChild(document.createTextNode(div[i].split(';')[x]));
+                tr.appendChild(td);
+              }
+        }
+      }
+      myTableDiv.appendChild(table);
+
+
     })
 }
 
 // FUNCAO ACHA CODIGO DO CLIENTE NA LISTA DE CLIENTESS
-function getClientByCode(code, list,len) {
+// function getClientByCode(code, list,len) {
+//     for (let i = 0; i < len; i++) {
+//         if (list[i].split(';')[0] == code) {
+//             console.log(list[i])
+//                 return list[i]
+//             }
+//         }
+//     }
+
+function getClientBySetor(code, list,len) {
+    var lista = []
     for (let i = 0; i < len; i++) {
-        if (list[i].split(';')[0] == code) {
-            console.log(list[i])
-                return list[i]
-            }
+        if (list[i].split(';')[2] == code) {
+            lista.push(list[i])  
+            } 
         }
+    return lista
     }
 
 
@@ -44,7 +75,8 @@ function show(text) {
     }
     else {
         text = text.split(';')
-        codigo.innerHTML = text[0]
+        console.log(text[0])
+        codigo.appendChild(document.createTextNode(codigo[0]))
         cliente.innerHTML = text[1]
         spaten.innerHTML = text[5]
         stella.innerHTML = text[6]
@@ -62,4 +94,25 @@ form.addEventListener("submit", (event) => {
     getvals()
 });
 
-
+function addTable() {
+    var myTableDiv = document.getElementById("myDynamicTable");
+  
+    var table = document.createElement('TABLE');
+    table.border = '1';
+  
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+  
+    for (var i = 0; i < 3; i++) {
+      var tr = document.createElement('TR');
+      tableBody.appendChild(tr);
+  
+      for (var j = 0; j < 7; j++) {
+        var td = document.createElement('TD');
+        td.width = '75';
+        td.appendChild(document.createTextNode("Cell " + i));
+        tr.appendChild(td);
+      }
+    }
+    myTableDiv.appendChild(table);
+  }
